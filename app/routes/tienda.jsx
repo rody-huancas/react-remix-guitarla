7d057e1561a1338c31b92554d7618c3954044c72
Rody Huancas <rodyhuancas.04@gmail.com>
2023-08-17 22:12:25 -0500

    Add: home page

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,7 +1,7 @@
 import { useLoaderData } from "@remix-run/react";
 import { getGuitarras } from "~/models/guitarras.server";
-import { Guitarra } from "../components/guitarra";
 import styles from "~/styles/guitarras.css";
+import { ListadoGuitarras } from "../components/listado-guitarras";
 
 export function meta() {
   return [
@@ -32,15 +32,7 @@ const Tienda = () => {
   return (
     <>
       <main className="contenedor">
-        <h2 className="heading">Nuestra colección</h2>
-
-        {guitarras.length && (
-          <div className="guitarras-grid">
-            {guitarras.map((guitarra) => (
-              <Guitarra key={guitarra?.id} guitarra={guitarra?.attributes} />
-            ))}
-          </div>
-        )}
+        <ListadoGuitarras guitarras={guitarras} />
       </main>
     </>
   );
